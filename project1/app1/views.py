66993d5ef50b12d149a3c54ffb383c429d402cb6
Alexandre Siedschlag <88800338+AlexandreSiedschlag@users.noreply.github.com>
2022-03-16 22:37:09 -0300

    1

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from urllib import request
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
 
 # Create your views here.
+def home(response):
+    return render(response, 'app1/templates/html.html')
